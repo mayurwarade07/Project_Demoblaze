6d4a996f92e9c2eb5c70923ad5ee4897703486ca
Yashingle210 <yashingle9137@gmail.com>
2026-05-30 11:25:18 +0530

    first commit

--- Project_DemoBlaze/src/main/java/base/BasePage.java
+++ Project_DemoBlaze/src/main/java/base/BasePage.java
@@ -1,7 +1,12 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasePage {
    private WebDriver driver;
+   
+   public void setUp() {
+   driver = new ChromeDriver();
+}
 }
